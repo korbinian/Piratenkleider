3ca561d5b97f07ff2e45148bc154beda2a555d12
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-11 23:54:59 +0200

    symbolbild eingeführt und struktur korrigiert

--- archive.php
+++ archive.php
@@ -1,27 +1,46 @@
 <?php get_header(); ?>
-
 <div class="section content">
-	<div class="row">
-		<div class="content-primary">
-			<div class="skin">
-				<?php if ( have_posts() ) the_post(); ?>
-				<h1>
-				<?php if ( is_day() ) : ?>
-					<?php printf( __( 'Tagesarchiv: %s', 'twentyten' ), get_the_date() ); ?>
-				<?php elseif ( is_month() ) : ?>
-					<?php printf( __( 'Monatsarchiv: %s', 'twentyten' ), get_the_date('F Y') ); ?>
-				<?php elseif ( is_year() ) : ?>
-					<?php printf( __( 'Jahresarchiv: %s', 'twentyten' ), get_the_date('Y') ); ?>
-				<?php else : ?>
-					<?php _e( 'Blogarchiv', 'twentyten' ); ?>
-				<?php endif; ?>
-				</h1>
-
-				<?php rewind_posts(); get_template_part( 'loop', 'archive' ); ?>
-			</div>
-		</div>
+  <div class="row">
+    <div class="content-primary">
+      <div class="content-header">
+          <div class="symbolbild"> 
+              <img src="<?php echo get_bloginfo('template_url'); ?>/images/tags-cats-suche.jpg" alt="" width="640" height="240" >
+               <div class="caption">  
+                  <?php if ( have_posts() ) the_post(); ?>
+                    <h2>
+                    <?php if ( is_day() ) : ?>
+                            <?php printf( __( 'Tagesarchiv: %s', 'twentyten' ), get_the_date() ); ?>
+                    <?php elseif ( is_month() ) : ?>
+                            <?php printf( __( 'Monatsarchiv: %s', 'twentyten' ), get_the_date('F Y') ); ?>
+                    <?php elseif ( is_year() ) : ?>
+                            <?php printf( __( 'Jahresarchiv: %s', 'twentyten' ), get_the_date('Y') ); ?>
+                    <?php else : ?>
+                            <?php _e( 'Blogarchiv', 'twentyten' ); ?>
+                    <?php endif; ?>
+                    </h2>
+               </div>   
+           </div>   
+      </div>
+        <div class="skin">
+            <?php rewind_posts(); get_template_part( 'loop', 'archive' ); ?>
 	</div>
+    </div>
+
+    <div class="content-aside">
+      <div class="skin">          
+        <?php 
+       if ( has_nav_menu( 'primary' ) ) {
+            wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary', 'walker'  => new My_Walker_Nav_Menu()) );      
+        } else { 
+        ?>
+          <ul class="menu">
+              <?php  wp_page_menu( ); ?>
+          </ul>
+         <?php } ?>
+         <?php get_sidebar(); ?>
+      </div>
+    </div>
+  </div>
 </div>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
