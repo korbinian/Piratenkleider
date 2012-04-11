bd573ba0ee69bf96215fd8e0b0de93686ba33ef5
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-11 23:56:01 +0200

    symbolbild eingeführt und struktur korrigiert

--- loop.php
+++ loop.php
@@ -1,105 +1,105 @@
-				<h1><?php printf( __( 'Pressearchiv: %s', 'twentyten' ), '' . single_cat_title( '', false ) . '' ); ?></h1>
-				<?php
-					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo '' . $category_description . ''; ?>	
-					<?php /* If there are no posts to display, such as an empty archive page */ ?>
-					<?php if ( ! have_posts() ) : ?>
-						<h1><?php _e( 'Nicht gefunden', 'twentyten' ); ?></h1>
-						<p><?php _e( 'Vielleicht hilft eine Suche weiter?', 'twentyten' ); ?></p>
-						<div class="fullwidth"><?php get_search_form(); ?></div>
-					<?php endif; ?>
+        <h1><?php printf( __( 'Archiv: %s', 'twentyten' ), '' . single_cat_title( '', false ) . '' ); ?></h1>
+        <?php
+                $category_description = category_description();
+                if ( ! empty( $category_description ) )
+                        echo '' . $category_description . ''; ?>	
+                <?php /* If there are no posts to display, such as an empty archive page */ ?>
+                <?php if ( ! have_posts() ) : ?>
+                        <h1><?php _e( 'Nichts gefunden', 'twentyten' ); ?></h1>
+                        <p><?php _e( 'Vielleicht hilft eine Suche weiter?', 'twentyten' ); ?></p>
+                        <div class="fullwidth"><?php get_search_form(); ?></div>
+                <?php endif; ?>
 
-					<?php while ( have_posts() ) : the_post(); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php /* gallery */ ?>
+                <?php /* gallery */ ?>
 
-					<?php if ( in_category( _x('gallery', 'gallery category slug', 'twentyten') ) ) : ?>
-						<h2>
-							<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
-								<?php the_title(); ?>
-							</a>
-						</h2>
-						<?php twentyten_posted_on(); ?>
+                <?php if ( in_category( _x('gallery', 'gallery category slug', 'twentyten') ) ) : ?>
+                        <h2>
+                                <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
+                                        <?php the_title(); ?>
+                                </a>
+                        </h2>
+                        <?php twentyten_posted_on(); ?>
 
-					<?php if ( post_password_required() ) : ?>
-						<?php the_content(); ?>
-					<?php else : ?>
-					<?php
-					$images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) );
-					$total_images = count( $images );
-					$image = array_shift( $images );
-					$image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );
-					?>
-						<a href="<?php the_permalink(); ?>"><?php echo $image_img_tag; ?></a>
+                <?php if ( post_password_required() ) : ?>
+                        <?php the_content(); ?>
+                <?php else : ?>
+                <?php
+                $images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) );
+                $total_images = count( $images );
+                $image = array_shift( $images );
+                $image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );
+                ?>
+                        <a href="<?php the_permalink(); ?>"><?php echo $image_img_tag; ?></a>
 
-						<p>
-							<?php printf( __( 'Diese Galerie enthält <a %1$s>%2$s photos</a>.', 'twentyten' ),
-										'href="' . get_permalink() . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"',
-										$total_images
-									); ?>
-						</p>
+                        <p>
+                                <?php printf( __( 'Diese Galerie enthält <a %1$s>%2$s photos</a>.', 'twentyten' ),
+                                                        'href="' . get_permalink() . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"',
+                                                        $total_images
+                                                ); ?>
+                        </p>
 
-						<?php the_excerpt(); ?>
-					<?php endif; ?>
+                        <?php the_excerpt(); ?>
+                <?php endif; ?>
 
-						<a href="<?php echo get_term_link( _x('gallery', 'gallery category slug', 'twentyten'), 'category' ); ?>" title="<?php esc_attr_e( 'View posts in the Gallery category', 'twentyten' ); ?>"><?php _e( 'More Galleries', 'twentyten' ); ?></a>
-								|
-								<?php comments_popup_link( __( 'Hinterlasse einen Kommentar', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?>
-								<?php edit_post_link( __( 'Bearbeiten', 'twentyten' ), '|', '' ); ?>
+                        <a href="<?php echo get_term_link( _x('gallery', 'gallery category slug', 'twentyten'), 'category' ); ?>" title="<?php esc_attr_e( 'View posts in the Gallery category', 'twentyten' ); ?>"><?php _e( 'More Galleries', 'twentyten' ); ?></a>
+                                        |
+                                        <?php comments_popup_link( __( 'Hinterlasse einen Kommentar', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?>
+                                        <?php edit_post_link( __( 'Bearbeiten', 'twentyten' ), '|', '' ); ?>
 
-					<?php /* asides */ ?>
+                <?php /* asides */ ?>
 
-					<?php elseif ( in_category( _x('asides', 'asides category slug', 'twentyten') ) ) : ?>
+                <?php elseif ( in_category( _x('asides', 'asides category slug', 'twentyten') ) ) : ?>
 
-						<?php if ( is_archive() || is_search() ) : // Display excerpts for archives and search. ?>
-							<?php the_excerpt(); ?>
-						<?php else : ?>
-							<?php the_content( __( 'Weiterlesen', 'twentyten' ) ); ?>
-						<?php endif; ?>
+                        <?php if ( is_archive() || is_search() ) : // Display excerpts for archives and search. ?>
+                                <?php the_excerpt(); ?>
+                        <?php else : ?>
+                                <?php the_content( __( 'Weiterlesen', 'twentyten' ) ); ?>
+                        <?php endif; ?>
 
-								<?php twentyten_posted_on(); ?>
-								|
-								<?php comments_popup_link( __( 'Hinterlasse einen Kommentar', 'twentyten' ), __( '1 Comment', 'Kommentar' ), __( '% Kommentare', 'twentyten' ) ); ?>
-								<?php edit_post_link( __( 'Bearbeiten', 'twentyten' ), '| ', '' ); ?>
+                                        <?php twentyten_posted_on(); ?>
+                                        |
+                                        <?php comments_popup_link( __( 'Hinterlasse einen Kommentar', 'twentyten' ), __( '1 Comment', 'Kommentar' ), __( '% Kommentare', 'twentyten' ) ); ?>
+                                        <?php edit_post_link( __( 'Bearbeiten', 'twentyten' ), '| ', '' ); ?>
 
-					<?php /* How to display all other posts. */ ?>
+                <?php /* How to display all other posts. */ ?>
 
-					<?php else : ?>
-							<h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-							<?php twentyten_posted_on(); ?>
+                <?php else : ?>
+                                <h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                                <?php twentyten_posted_on(); ?>
 
-					<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
-							<?php the_excerpt(); ?>
-					<?php else : ?>
-							<?php the_content( __( 'Weiterlesen', 'twentyten' ) ); ?>
-							<?php wp_link_pages( array( 'before' => '' . __( 'Seiten:', 'twentyten' ), 'after' => '' ) ); ?>
-					<?php endif; ?>
+                <?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
+                                <?php the_excerpt(); ?>
+                <?php else : ?>
+                                <?php the_content( __( 'Weiterlesen', 'twentyten' ) ); ?>
+                                <?php wp_link_pages( array( 'before' => '' . __( 'Seiten:', 'twentyten' ), 'after' => '' ) ); ?>
+                <?php endif; ?>
 
-								<?php if ( count( get_the_category() ) ) : ?>
-									<?php printf( __( 'veröffentlicht unter  %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
-									|
-								<?php endif; ?>
-								<?php
-									$tags_list = get_the_tag_list( '', ', ' );
-									if ( $tags_list ):
-								?>
-									<?php printf( __( 'Tagged %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
-									|
-								<?php endif; ?>
-								<?php comments_popup_link( __( 'Hinterlasse einen Kommentar', 'twentyten' ), __( '1 Kommentar', 'twentyten' ), __( '% Kommentare', 'twentyten' ) ); ?>
-								<?php edit_post_link( __( 'Bearbeiten', 'twentyten' ), '| ', '' ); ?>
+                                        <?php if ( count( get_the_category() ) ) : ?>
+                                                <?php printf( __( 'veröffentlicht unter  %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+                                                |
+                                        <?php endif; ?>
+                                        <?php
+                                                $tags_list = get_the_tag_list( '', ', ' );
+                                                if ( $tags_list ):
+                                        ?>
+                                                <?php printf( __( 'Tagged %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+                                                |
+                                        <?php endif; ?>
+                                        <?php comments_popup_link( __( 'Hinterlasse einen Kommentar', 'twentyten' ), __( '1 Kommentar', 'twentyten' ), __( '% Kommentare', 'twentyten' ) ); ?>
+                                        <?php edit_post_link( __( 'Bearbeiten', 'twentyten' ), '| ', '' ); ?>
 
-						<?php comments_template( '', true ); 
-						?>
-						
-					<?php endif; // This was the if statement that broke the loop into three parts based on categories. ?>
-					<?php echo "<hr />"; ?>
+                        <?php comments_template( '', true ); 
+                        ?>
 
-					<?php endwhile; // End the loop. Whew. ?>
+                <?php endif; // This was the if statement that broke the loop into three parts based on categories. ?>
+                <?php echo "<hr />"; ?>
 
-					<?php /* Display navigation to next/previous pages when applicable */ ?>
-					<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-								<?php next_posts_link( __( '&larr; Ältere Beiträge', 'twentyten' ) ); ?>
-								<?php previous_posts_link( __( 'Neuere Beiträge &rarr;', 'twentyten' ) ); ?>
-					<?php endif; ?>
+                <?php endwhile; // End the loop. Whew. ?>
+
+                <?php /* Display navigation to next/previous pages when applicable */ ?>
+                <?php if (  $wp_query->max_num_pages > 1 ) : ?>
+                                        <?php next_posts_link( __( '&larr; Ältere Beiträge', 'twentyten' ) ); ?>
+                                        <?php previous_posts_link( __( 'Neuere Beiträge &rarr;', 'twentyten' ) ); ?>
+                <?php endif; ?>
