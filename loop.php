23e643b3f4689daaf846d4a292d43990aa194db1
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-11 23:57:16 +0200

    h1 raus

--- loop.php
+++ loop.php
@@ -1,4 +1,4 @@
-        <h1><?php printf( __( 'Archiv: %s', 'twentyten' ), '' . single_cat_title( '', false ) . '' ); ?></h1>
+
         <?php
                 $category_description = category_description();
                 if ( ! empty( $category_description ) )
