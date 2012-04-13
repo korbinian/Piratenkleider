8e65d25ca4fe3483217df5ab664d1c5db2a63e0c
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-14 00:32:40 +0200

    formatierung

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,7 +1,7 @@
 <div class="first-footer-widget-area">
-	<div class="skin">
-            <?php if ( is_active_sidebar( 'first-footer-widget-area' ) ) : ?>
-            	<?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
-	    <?php endif; ?>
-	</div>
+    <div class="skin">
+        <?php if ( is_active_sidebar( 'first-footer-widget-area' ) ) : ?>
+            <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
+        <?php endif; ?>
+    </div>
 </div>
