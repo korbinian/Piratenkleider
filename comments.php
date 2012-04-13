f4bdeed3bfb7e9255649af27bf02dce1905abb8d
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-13 15:57:28 +0200

    Kommentarhinweis schoener und unnoetiges raus

--- comments.php
+++ comments.php
@@ -1,30 +1,29 @@
 <?php if ( post_password_required() ) : ?>
-<p><?php _e( 'Dieser Artikel ist Passwortgeschützt. Bitte gib das Passwort ein um die Kommentare zu sehen.', 'twentyten' ); ?></p>
-<?php 
-	return;
+    <p>Dieser Artikel ist Passwortgeschützt. Bitte gib das Passwort ein um die Kommentare zu sehen.</p>
+    <?php return;
 endif; ?>
 
 <?php if ( have_comments() ) : ?>
-<h3 id="comments-title">
-<?php printf( _n( 'Ein Kommentar zu %2$s', '%1$s Kommentare zu %2$s', get_comments_number(), 'twentyten' ), number_format_i18n( get_comments_number() ), '' . get_the_title() . '' ); ?>
-</h3>
+    <h3 id="comments-title">
+    <?php printf( _n( 'Ein Kommentar zu %2$s', '%1$s Kommentare zu %2$s', get_comments_number(), 'twentyten' ), number_format_i18n( get_comments_number() ), '' . get_the_title() . '' ); ?>
+    </h3>
 
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :  ?>
-	<?php previous_comments_link( __( '&larr; Ältere Kommentare', 'twentyten' ) ); ?>
-	<?php next_comments_link( __( 'Neuere Kommentare &rarr;', 'twentyten' ) ); ?>
-<?php endif; ?>
-<ol>
-	<?php wp_list_comments( array( 'callback' => 'twentyten_comment' ) );
-	?>
-</ol>
+    <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :  ?>
+            <?php previous_comments_link( __( '&larr; Ältere Kommentare', 'twentyten' ) ); ?>
+            <?php next_comments_link( __( 'Neuere Kommentare &rarr;', 'twentyten' ) ); ?>
+    <?php endif; ?>
+    <ol>
+            <?php wp_list_comments( array( 'callback' => 'twentyten_comment' ) );
+            ?>
+    </ol>
 
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :  ?>
-	<?php previous_comments_link( __( '&larr; Ältere Kommentare', 'twentyten' ) ); ?>
-	<?php next_comments_link( __( 'Neuere Kommentare &rarr;', 'twentyten' ) ); ?>
-<?php endif; ?>
+    <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :  ?>
+            <?php previous_comments_link( __( '&larr; Ältere Kommentare', 'twentyten' ) ); ?>
+            <?php next_comments_link( __( 'Neuere Kommentare &rarr;', 'twentyten' ) ); ?>
+    <?php endif; ?>
 
 <?php else : if ( ! comments_open() ) : ?>
-	<p><?php _e( 'Die Kommentarfunktion ist gesperrt', 'twentyten' ); ?></p>
+	<p>Das Kommentieren dieses Artikels ist nicht (mehr) möglich.</p>
 <?php endif; ?>
 
 <?php endif; ?>
--- sidebar-header.php
+++
@@ -1,5 +0,0 @@
-<div class="header-widget-area">
-<?php if ( is_active_sidebar( 'header-widget-area' ) ) : ?>
-<?php dynamic_sidebar( 'header-widget-area' ); ?>
-<?php endif; ?>
-</div>
