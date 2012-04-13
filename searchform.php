f6a10523f77e05c9f2c96d73967b8d67f386a1e4
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-14 00:32:23 +0200

    formatierung

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <h2 class="visuallyhidden">Suche</h2>
+
 <form method="get" class="searchform" action="<?php bloginfo('url'); ?>/">
 	<label class="visuallyhidden" for="s">Suchen:</label>
 	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="Suchbegriff&hellip;" />
