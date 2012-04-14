088766a5a63c2f9cf4d357cb2c3cc2f8d8950c2d
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-15 00:39:57 +0200

    fallback.text mit den links zu allen websites der LVs und des Bundes

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,7 +1,28 @@
 <div class="first-footer-widget-area">
     <div class="skin">
-        <?php if ( is_active_sidebar( 'first-footer-widget-area' ) ) : ?>
+        <?php if ( is_active_sidebar( 'first-footer-widget-area' ) ) { ?>
             <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
-        <?php endif; ?>
+        <?php } else { ?>
+            
+        <h3>Piratenpartei Deutschland <span style="display: inline; float: right; font-size: 0.8em;"><a href="http://www.piratenpartei.de">www.piratenpartei.de</a></span></h3>
+            <ul>
+                <li><a href="http://www.piratenpartei-bw.de/">Baden-Württemberg</a></li>   
+                <li><a href="http://www.piratenpartei-bayern.de/">Bayern</a></li>   
+                <li><a href="http://berlin.piratenpartei.de/">Berlin</a></li>   
+                <li><a href="http://www.piratenbrandenburg.de/">Brandenburg</a></li>   
+                <li><a href="http://bremen.piratenpartei.de/">Bremen </a></li>   
+                <li><a href="http://www.piratenpartei-hamburg.de">Hamburg</a></li>   
+                <li><a href="http://www.piratenpartei-hessen.de/">Hessen</a></li>   
+                <li><a href="http://www.piratenpartei-mv.de/">Mecklenburg-Vorpommern</a></li>   
+                <li><a href="http://www.piratenpartei-niedersachsen.de/">Niedersachsen</a></li>   
+                <li><a href="https://www.piratenpartei-nrw.de/">Nordrhein-Westfalen</a></li>   
+                <li><a href="http://www.piraten-rlp.de/">Rheinland-Pfalz</a></li>   
+                <li><a href="http://www.piratenpartei-saarland.de/">Saarland</a></li>   
+                <li><a href="http://www.piraten-sachsen.de/">Sachsen</a></li>   
+                <li><a href="http://www.piraten-lsa.de/">Sachsen-Anhalt</a></li>   
+                <li><a href="http://www.piratenpartei-sh.de/">Schleswig-Holstein</a></li>   
+                <li><a href="http://www.piraten-thueringen.de/">Thüringen</a></li>   
+            </ul>
+        <?php } ?>    
     </div>
 </div>
