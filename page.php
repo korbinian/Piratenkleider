68d979457a644ed21b3bb1604fe75610b8672e1a
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-18 20:19:48 +0200

    aktiv-defaultseitenbild definiert, damit bei seiten die wahl ermoeglicht wird, ob diese immer auch bilder haben sollen

--- page.php
+++ page.php
@@ -1,20 +1,45 @@
-<?php get_header(); ?>
+<?php get_header();
+global $defaultoptions;
+$options = get_option( 'piratenkleider_theme_options' );
+if (!isset($options['aktiv-defaultseitenbild'])) 
+            $options['aktiv-defaultseitenbild'] = $defaultoptions['aktiv-defaultseitenbild'];
+?>
 
 <div class="section content">
   <div class="row">
     <div class="content-primary">
-      <div class="content-header">
-        <h1 id="page-title"><span><?php the_title(); ?></span></h1>
-        <?php if (has_post_thumbnail()) {
-            the_post_thumbnail(); 
+     
+        <?php if (has_post_thumbnail()) { ?>
+          <div class="content-header">
+             <div class="symbolbild"> 
+                 <?php  the_post_thumbnail();  ?>
+                <div class="caption">  
+                   <h2><?php the_title(); ?></h2>
+                </div>   
+            </div>
+          </div>
+        <?php 
         } else {            
-           $bilderoptions = get_option( 'piratenkleider_theme_defaultbilder' ); 
-           $defaultbildsrc = $bilderoptions['seiten-defaultbildsrc'];                        
-            echo '<img src="'.$defaultbildsrc.'" width="640" height="240" alt="">';                        
+           if ($options['aktiv-defaultseitenbild']==1) {
+            ?>
+                <div class="content-header">
+                <div class="symbolbild"> 
+                <?php     
+                $bilderoptions = get_option( 'piratenkleider_theme_defaultbilder' ); 
+                $defaultbildsrc = $bilderoptions['seiten-defaultbildsrc'];                        
+                echo '<img src="'.$defaultbildsrc.'" width="640" height="240" alt="">';                        
+                ?>
+                <div class="caption">  
+                    <h2><?php the_title(); ?></h2>
+                </div>   
+                </div></div>
+                <?php 
+           }            
         }   
          ?>
-      </div>
+      
       <div class="skin">
+         <h1 id="page-title"><span><?php the_title(); ?></span></h1>
         <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
         <?php the_content(); ?>
         <?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
