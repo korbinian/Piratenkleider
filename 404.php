5aca4acc5fd7f37cbf624edce3ce863e058847b6
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-23 23:21:08 +0200

    Symbolbild ausgetauscht

--- 404.php
+++ 404.php
@@ -1,15 +1,22 @@
-<?php get_header(); ?>
+<?php get_header(); 
+ $options = get_option( 'piratenkleider_theme_options' );
+  if (!isset($options['src-default-symbolbild-big'])) 
+            $options['src-default-symbolbild-big'] = $defaultoptions['src-default-symbolbild-big'];
+ ?>
 
 <div class="section content">
   <div class="row">
     <div class="content-primary">
-      <div class="content-header">
+      <div class="content-header-big">
           <h1>Seite nicht gefunden</h1>
          <div class="symbolbild">                   
-              <img src="<?php echo get_bloginfo('template_url'); ?>/images/404.png" alt=""  >
+              <img src="<?php echo $options['src-default-symbolbild-big']?>" alt="" >
                <div class="caption">  
                    <p style="font-size: 2em;" class="bebas">404</p>                  
                </div>   
+              <div class="aaarh">
+                  <p>AAARH!<br>Ihr werdet sie nicht finden!</p>
+              </div>
            </div> 
          
       </div>
