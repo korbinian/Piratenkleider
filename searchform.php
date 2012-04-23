bade59d4ac728cb5dfa12ae9f59b0973a805dce1
Wolfgang Wiese <xwolf@xwolf.de>
2012-04-23 23:22:24 +0200

    js-ersatztext

--- searchform.php
+++ searchform.php
@@ -2,6 +2,8 @@
 
 <form method="get" class="searchform" action="<?php bloginfo('url'); ?>/">
 	<label class="visuallyhidden" for="s">Suchen:</label>
-	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="Suchbegriff&hellip;" />
+	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="Suchbegriff eingeben" 
+            value="Suchbegriff eingeben"   
+            onfocus="if(this.value=='Suchbegriff eingeben')this.value='';" onblur="if(this.value=='')this.value='Suchbegriff eingeben';" />
 	<input type="submit" class="searchsubmit" value="suchen" />
 </form>
