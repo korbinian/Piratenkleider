0b6549a4b5287a9ce452ea693a516f19c06364fc
Wolfgang Wiese <xwolf@xwolf.de>
2012-07-04 23:24:33 +0200

    relative urls

--- functions.php
+++ functions.php
@@ -871,8 +871,48 @@ function wpi_linkexternclass_callback($matches)
 add_filter('the_content', 'wpi_linkexternclass');
 
 
+add_action( 'template_redirect', 'rw_relative_urls' );
+    function rw_relative_urls() {
+        // Don't do anything if:
+        // - In feed
+        // - In sitemap by WordPress SEO plugin
+        if ( is_feed() || get_query_var( 'sitemap' ) )
+        return;
+        $filters = array(
+            'post_link',
+            'post_type_link',
+            'page_link',
+            'attachment_link',
+            'get_shortlink',
+            'post_type_archive_link',
+            'get_pagenum_link',
+            'get_comments_pagenum_link',
+            'term_link',
+            'search_link',
+            'day_link',
+            'month_link',
+            'year_link',
+        );
+        foreach ( $filters as $filter )
+        {
+         add_filter( $filter, 'wp_make_link_relative' );
+        }
+    }
 
+ function wpi_relativeurl($content){
+        return preg_replace_callback('/<a[^>]+/', 'wpi_relativeurl_callback', $content);
+    }
+ 
+function wpi_relativeurl_callback($matches) {
 
+        $link = $matches[0];
+        $site_link = get_bloginfo('url');
+        $link = preg_replace("%href=\"$site_link%i", 'href="', $link); 
+                
+        return $link;
+    }
+add_filter('the_content', 'wpi_relativeurl');
+   
 
 function dimox_breadcrumbs() {
   global $defaultoptions;
