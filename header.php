e837eb8c32e2d4816f858db21f129d5d8a8df69e
Wolfgang Wiese <xwolf@xwolf.de>
2014-04-16 20:36:11 +0200

    home_url relative; and stays on chosen protocol

--- header.php
+++ header.php
@@ -89,7 +89,7 @@
                     if ( $options['aktiv-suche'] == "1" ){ ?>
                         <div id="searchform">
                         <h2 class="skip"><?php _e("Suche", 'piratenkleider'); ?></h2>
-                        <form method="get" class="searchform" action="<?php echo home_url(); ?>/">
+                        <form method="get" class="searchform" action="<?php echo home_url('','relative'); ?>/">
                                 <label class="visuallyhidden" for="s"><?php _e("Suche nach", 'piratenkleider'); ?>:</label>
                                 <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="<?php _e("Suchbegriff eingeben", 'piratenkleider'); ?>"  
                                     onfocus="if(this.value=='<?php _e("Suchbegriff eingeben", 'piratenkleider'); ?>')this.value='';" onblur="if(this.value=='')this.value='<?php _e("Suchbegriff eingeben", 'piratenkleider'); ?>';" />
--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 
     <h2 class="skip"><a name="searchform"><?php _e("Suche", 'piratenkleider'); ?></a></h2>
 
-    <form method="get" class="searchform" action="<?php echo home_url(); ?>/">
+    <form method="get" class="searchform" action="<?php echo home_url('','relative'); ?>/">
             <label class="visuallyhidden" for="suche"><?php _e("Suche nach", 'piratenkleider'); ?>:</label>
             <input type="text" value="<?php the_search_query(); ?>" name="s" id="suche" placeholder="<?php _e("Suchbegriff eingeben", 'piratenkleider'); ?>"  
                 onfocus="if(this.value=='<?php _e("Suchbegriff eingeben", 'piratenkleider'); ?>')this.value='';" onblur="if(this.value=='')this.value='<?php _e("Suchbegriff eingeben", 'piratenkleider'); ?>';" />
